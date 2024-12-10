370ad72239171f50dd6bbfa296edb43bbb256456
Nicolò Gandini <126022150+nicologandini@users.noreply.github.com>
2024-12-10 21:39:50 +0100

    hide object from script

--- Assets/Scripts/GameLogic/Game.cs
+++ Assets/Scripts/GameLogic/Game.cs
@@ -3,21 +3,11 @@ using System.Collections.Generic;
 
 public class Game : MonoBehaviour
 {
-    //private Transform childObj;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // childObj = transform.find("Balloon_station_plate");
-        // childObj.active = false;
-        GameObject s0 = GameObject.Find("Balloon_station_0");
-        // GameObject myResults = s0.GetComponentInChildren<GameObject>();
-        // Debug.Log(myResults);
-        s0.SetActive(false);
+        GameObject s1 = GameObject.Find("Balloon_station_1");
+        Transform childTransform = s1.transform.Find("Balloon_station_plate");
+        childTransform.gameObject.SetActive(false);
     }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     
-    // }
 }
