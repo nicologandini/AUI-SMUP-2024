e18854b65c06bbb58a26139559822217cd0e585d
Luca <luca4.proserpio@mail.polimi.it>
2025-01-13 18:10:20 +0100

    Added 2 tables, one for each player + cooldown for request match

--- Assets/Scripts/Multiplayer/GameLogic_Multiplayer/AutoMoveBalloons.cs
+++ Assets/Scripts/Multiplayer/GameLogic_Multiplayer/AutoMoveBalloons.cs
@@ -3,26 +3,48 @@ using UnityEngine;
 public class AutoMoveBalloons : MonoBehaviour
 {
     [SerializeField] private GameObject[] balloons;
-    [SerializeField] private GameObject[] deliverySpot;
+    [SerializeField] private GameObject[] deliverySpots;
 
 
     [SerializeField] private bool sortAtStart = false;
+    public bool SortAtStart => sortAtStart;
 
 
     private void Start() {
-        if (sortAtStart) {
-            AutoSortBalloons();
-        }
+        // if (sortAtStart) {
+        //     AutoSortBalloons();
+        // }
     }
 
     public void AutoSortBalloons() {
-        if(balloons.Length != deliverySpot.Length) {
+        if(balloons.Length != deliverySpots.Length) {
             Debug.Log("Different number of balloons and delivery spots!!");
             return;
         }
 
         for (int i = 0; i<balloons.Length; i++) {
-            balloons[i].transform.position = deliverySpot[i].transform.position + Vector3.up*0.1f;
+            balloons[i].transform.position = deliverySpots[i].transform.position + Vector3.up*0.1f;
+        }
+    }
+
+    public void SetBalloons (GameObject[] p_balloons){
+        balloons = new GameObject[p_balloons.Length];
+
+        for (int i = 0; i<balloons.Length; i++) {
+            balloons[i] = p_balloons[i];
+        }
+    }
+
+    public void SetDeliverySpots(GameObject[] p_spots) {
+        deliverySpots = new GameObject[p_spots.Length]; 
+
+        Console_UI.print($"spots lenght: {p_spots.Length}");
+        foreach(GameObject obj in p_spots) {
+            Console_UI.print($"spot: {obj}");
+        }
+
+        for (int i = 0; i<deliverySpots.Length; i++) {
+            deliverySpots[i] = p_spots[i];
         }
     }
 }
--- Assets/Scripts/Multiplayer/GameLogic_Multiplayer/GameScript_Multiplayer.cs
+++ Assets/Scripts/Multiplayer/GameLogic_Multiplayer/GameScript_Multiplayer.cs
@@ -9,8 +9,21 @@ public class GameMultiplayer : MonoBehaviour
 {
     Player player = null;
 
-    [SerializeField] List <GameObject> stationsPlayer = null;  // Inputs in Unity
-    [SerializeField] List <GameObject> balloonsPlayer = null; // Inputs in Unity
+    [Header("Master player ref")]
+    [SerializeField] List <GameObject> stationsPlayer1 = null;      // Inputs in Unity
+    [SerializeField] List <GameObject> balloonsPlayer1 = null;      // Inputs in Unity
+    [Header("Guest player ref")]
+    [SerializeField] List <GameObject> stationsPlayer2 = null;      // Inputs in Unity
+    [SerializeField] List <GameObject> balloonsPlayer2 = null;      // Inputs in Unity
+
+    [Header("Utils")]
+    [SerializeField] DisableOtherPlayerObjects disabler;
+    [SerializeField] AutoMoveBalloons autoMoveBalloons;
+    [SerializeField] RequestMatchHandler requestMatchHandler;
+
+
+    List <GameObject> _stationsPlayer = null;  
+    List <GameObject> _balloonsPlayer = null; 
 
     int balloons_counter = 0;
 
@@ -20,24 +33,60 @@ public class GameMultiplayer : MonoBehaviour
     void Start()
     {
         GameInstance = this;
-        
-        if(stationsPlayer.Count != stationsPlayer.Count){
-            Debug.Log("Wrong number of balloons or statios!");
-        } else {
-            balloons_counter = balloonsPlayer.Count;
+        if (disabler == null) {
+            disabler = FindFirstObjectByType<DisableOtherPlayerObjects>();
+        }
+        if (autoMoveBalloons == null) {
+            autoMoveBalloons = GetComponent<AutoMoveBalloons>();
+        }
+        if (requestMatchHandler == null) {
+            requestMatchHandler = FindFirstObjectByType<RequestMatchHandler>();
         }
 
-        player = new Player(balloonsPlayer, stationsPlayer);
+        if(PhotonNetwork.IsMasterClient) {
+            _stationsPlayer = stationsPlayer1;
+            _balloonsPlayer = balloonsPlayer1;
+
+            disabler.DisableObjects(stationsPlayer2);
+            disabler.DisableObjects(balloonsPlayer2);
+        } else {
+            _stationsPlayer = stationsPlayer2;
+            _balloonsPlayer = balloonsPlayer2;
+
+            disabler.DisableObjects(stationsPlayer1);
+            disabler.DisableObjects(balloonsPlayer1);
+        }
+
+        if (autoMoveBalloons.SortAtStart) {
+            autoMoveBalloons.SetBalloons(_balloonsPlayer.ToArray<GameObject>());
+            autoMoveBalloons.SetDeliverySpots(_stationsPlayer.ToArray<GameObject>());
+
+            autoMoveBalloons.AutoSortBalloons();
+        }
+        
+        if(_stationsPlayer.Count != _stationsPlayer.Count){
+            Debug.Log("Wrong number of balloons or statios!");
+        } else {
+            balloons_counter = _balloonsPlayer.Count;
+        }
+
+        player = new Player(_balloonsPlayer, _stationsPlayer);
         Player p = player;
 
+        requestMatchHandler.SetPhotonView(PhotonView.Get(this));
+        requestMatchHandler.SetPlayer(player);
+
+        //print platforms info
         for(int i=0; i<p.getStations().Count; i++) {
             Debug.Log("Stazione " + p.getStations()[i] + " " + p.getStations()[i].GetInstanceID() + " - " + p.getBalloons()[i] + " " + p.getBalloons()[i].GetInstanceID());
         }
 
-        // Pretend to press the button
-        if (!PhotonNetwork.IsMasterClient) {
-            Invoke("RequestMatch", 5f);
-        }
+        // // Pretend to press the button
+        // if (PhotonNetwork.IsMasterClient) {
+        //     FakeButtonPress();
+        // } else {
+        //     Invoke("FakeButtonPress", 11f);
+        // }
 
         Console_UI.Instance.ClearLog();
     }
@@ -77,15 +126,8 @@ public class GameMultiplayer : MonoBehaviour
         Debug.Log("Situation on " + station + ": " + p.getBalloon(station));
     }
 
-    public void RequestMatch() {
-        Console_UI.Instance.ConsolePrint("Requesting mathc");
 
-        PhotonView photonView = PhotonView.Get(this);
-        photonView.RPC("performMatch", RpcTarget.Others, player.GetBalloonsColors());        
-    }   
-
-    [PunRPC]  
-    private void performMatch(string colorNames){
+    public void performMatch(string colorNames){
         Console_UI.Instance.ConsolePrint("Colors Name: " + colorNames);
         List<string> colorsList = GetStrings(colorNames);
 
@@ -105,27 +147,6 @@ public class GameMultiplayer : MonoBehaviour
             Console_UI.Instance.ConsolePrint("Not a match!");
             return;
         }
-
-        /*
-        foreach(Player p in players){
-            if(p.getDeliveredBalloons() is null){
-                Debug.Log("Not all the stations are occupied by a balloon! Match not possible");
-                return;
-            }
-            matrix.Add(p.getDeliveredBalloons());
-        }
-
-        for(int i=0; i<matrix.Count-1; i++){  // Iterate thru columns
-            for(int j=0; j<matrix[i].Count; j++){  // Iterate thru rows
-                Material mat1 = matrix[j][i].GetComponent<Renderer>().material;
-                Material mat2 = matrix[j][i+1].GetComponent<Renderer>().material;
-                Debug.Log(mat1 + " - " + mat2);
-                if(mat1 != mat2){  // Material of the balloons must be the same.
-                    Debug.Log("Not a match!");
-                    return;
-                }
-            }
-        }*/
 
         PhotonView photonView = PhotonView.Get(this);
         photonView.RPC("Win", RpcTarget.All);  
@@ -178,125 +199,7 @@ public class GameMultiplayer : MonoBehaviour
         Console_UI.Instance.ConsolePrint("All matches!", 40);
     }
 
-
-
-
-
-
-
-    // public void addBalloon_old(bool position, Material mat, GameObject station) // true = right, false = left
-    // {
-    //     Debug.Log(mat.name);
-    //     int stationIndex = returnStationIndex(station);
-    //     int color = returnColor(mat);
-
-        
-    //     if (position) {
-    //         if (deliveredBalloons_R[stationIndex] == 11) // se la stazione non e' gia' occupata entra nell'if
-    //         {
-    //             //Debug.Log(mat.name);
-    //             deliveredBalloons_R[stationIndex] = color;
-    //             Debug.Log("aggiunto palloncino di colore " + mat.name + " in posizione " + (stationIndex + 1));
-    //         }
-    //     } else {
-    //         if (deliveredBalloons_L[stationIndex] == 11) // se la stazione non e' gia' occupata entra nell'if
-    //         {
-    //             //Debug.Log(deliveredBalloons_L.Length);
-    //             deliveredBalloons_L[stationIndex] = color;//mat.name;
-    //             Debug.Log("aggiunto palloncino di colore " + mat.name + " in posizione " + (stationIndex + 1));
-    //         }
-    //     }
-        
-    // }
-
-    // public void removeBalloon_old(bool position, Material mat, GameObject station)
-    // {
-    //     int stationIndex = returnStationIndex(station);
-    //     int color = returnColor(mat);
-
-    //     if (position) {
-    //         deliveredBalloons_R[color] = 11; // ora libero
-    //         Debug.Log("rimosso palloncino di colore " + mat.name + " in posizione " + (stationIndex + 1));
-    //     }
-    //     else {
-    //         deliveredBalloons_L[color] = 11; // ora libero
-    //         Debug.Log("rimosso palloncino di colore " + mat.name + " in posizione " + (stationIndex + 1));
-    //         Debug.Log(deliveredBalloons_L[color]);
-    //     }
-
-    // }
-
-    // public void performMatch_old()
-    // {
-    //     // Itera per tutti gli elementi degli array deliveredBalloons, che in ogni posizione (stazione) contengono gli indici relativi ai colori
-    //     for (int i = 0; i < 6; i++)
-    //     {
-    //         if (deliveredBalloons_L[i] != deliveredBalloons_R[i] || deliveredBalloons_L[i] == 11 || deliveredBalloons_R[i] == 11) // valori devono essere uguali e nessuna delle due stazioni vuota
-    //         {
-    //             Debug.Log("not a match");
-
-    //             // Da implementare il ritorno dei palloncini ai posti di partenza
-
-    //             return;
-    //         }
-    //     }
-
-    //     Debug.Log("match!");
-    //     return;
-    // }
-
-    // private int returnStationIndex(GameObject station)
-    // {
-    //     if (station.name == "Balloon_station_1" || station.name == "Balloon_station_1_R")
-    //     {
-    //         return 0;
-    //     } else if (station.name == "Balloon_station_2" || station.name == "Balloon_station_2_R")
-    //     {
-    //         return 1;
-    //     }
-    //     else if (station.name == "Balloon_station_3" || station.name == "Balloon_station_3_R")
-    //     {
-    //         return 2;
-    //     }
-    //     else if (station.name == "Balloon_station_4" || station.name == "Balloon_station_4_R")
-    //     {
-    //         return 3;
-    //     }
-    //     else if (station.name == "Balloon_station_5" || station.name == "Balloon_station_5_R")
-    //     {
-    //         return 4;
-    //     }
-    //     else if (station.name == "Balloon_station_6" || station.name == "Balloon_station_6_R")
-    //     {
-    //         return 5;
-    //     } else
-    //     {
-    //         return 0;
-    //     }
-
-    // }
-
-    // private int returnColor(Material mat)
-    // {
-    //     switch(mat.name)
-    //     {
-    //         case "blue":
-    //             return (int)colors.blue;
-    //         case "green":
-    //             return (int)colors.green;
-    //         case "lightblue":
-    //             return (int)colors.lightblue;
-    //         case "orange":
-    //             return (int)colors.orange;
-    //         case "pink":
-    //             return (int)colors.pink;
-    //         case "purple (Instance)":
-    //             return (int)colors.purple;
-    //         default:
-    //             Debug.Log("error: no color");
-    //             return 40; // costante simbolica
-
-    //     }
-
-    // }
+    private void FakeButtonPress() {
+        requestMatchHandler.RequestMatch();
+    }
 }
