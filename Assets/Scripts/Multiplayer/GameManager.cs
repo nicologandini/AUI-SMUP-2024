46edf43c2616930baf4a4e4bd54ee3530381b25c
Luca <luca4.proserpio@mail.polimi.it>
2025-02-06 12:34:31 +0100

    Main Menu + scaled down objects

--- Assets/Scripts/Multiplayer/GameManager.cs
+++ Assets/Scripts/Multiplayer/GameManager.cs
@@ -17,7 +17,7 @@ namespace SMUP.Multiplayer
                 return;
             }
             Console_UI.Instance.ConsolePrint("PhotonNetwork : Loading scene");
-            PhotonNetwork.LoadLevel("MicTest");
+            PhotonNetwork.LoadLevel("Outside_Final");
         }
 
     #endregion
